0dc36775600d0125014294e4e560e676bd746f6e
Shavonn <shavonn.brown@gmail.com>
2018-02-18 14:45:26 -0500

    error, server listener in goroutine

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"revelforce/cmd/web"
 	"revelforce/internal/platform/db"
 	"revelforce/internal/platform/session"
+	"sync"
 	"time"
 
 	"github.com/spf13/viper"
@@ -38,5 +42,32 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	log.Fatalln(srv.ListenAndServe())
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		log.Printf("startup : Listening %s", viper.GetString("addr"))
+		log.Printf("shutdown : Listener closed : %v", srv.ListenAndServe())
+		wg.Done()
+	}()
+
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, os.Interrupt)
+
+	<-osSignals
+
+	const timeout = 5 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("shutdown : Graceful shutdown did not complete in %v : %v", timeout, err)
+
+		if err := srv.Close(); err != nil {
+			log.Printf("shutdown : Error killing server : %v", err)
+		}
+	}
+
+	wg.Wait()
+	log.Println("main : Completed")
 }
