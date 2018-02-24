682134da0d98085adbbbfe1825660c5456e0ac23
Shavonn <shavonn.brown@gmail.com>
2018-02-24 18:59:49 -0500

    git rid of wait group

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,15 @@ import (
 	"revelforce/cmd/web"
 	"revelforce/internal/platform/db"
 	"revelforce/internal/platform/session"
-	"sync"
+	"syscall"
 	"time"
 
 	"github.com/spf13/viper"
 )
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+}
 
 func main() {
 	err := web.LoadConfig()
@@ -21,9 +25,10 @@ func main() {
 		log.Fatalf("Load Config : %v", err)
 	}
 
+	log.Println("main : Started : Initialize MySql")
 	masterDB, err := db.GetConnection()
 	if err != nil {
-		log.Fatalf("Connect DB : %v", err)
+		log.Fatalf("startup : Connect to DB : %v", err)
 	}
 	defer masterDB.Close()
 
@@ -38,36 +43,37 @@ func main() {
 		Handler:        sesh.Use(web.Routes()),
 		IdleTimeout:    time.Minute,
 		ReadTimeout:    5 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		WriteTimeout:   5 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	serverErrors := make(chan error, 1)
 
 	go func() {
 		log.Printf("startup : Listening %s", viper.GetString("addr"))
-		log.Printf("shutdown : Listener closed : %v", srv.ListenAndServe())
-		wg.Done()
+		serverErrors <- srv.ListenAndServe()
 	}()
 
 	osSignals := make(chan os.Signal, 1)
-	signal.Notify(osSignals, os.Interrupt)
+	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
 
-	<-osSignals
+	log.Println("main : Start shutdown...")
 
-	const timeout = 5 * time.Second
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error starting server: %v", err)
 
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Printf("shutdown : Graceful shutdown did not complete in %v : %v", timeout, err)
+	case <-osSignals:
+		ctx, cancel := context.WithTimeout(context.Background(), (5 * time.Second))
+		defer cancel()
 
-		if err := srv.Close(); err != nil {
-			log.Printf("shutdown : Error killing server : %v", err)
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("Graceful shutdown did not complete in %v : %v", (5 * time.Second), err)
+			if err := srv.Close(); err != nil {
+				log.Fatalf("Could not stop http server: %v", err)
+			}
 		}
 	}
 
-	wg.Wait()
 	log.Println("main : Completed")
 }
